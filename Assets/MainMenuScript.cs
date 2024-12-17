a24cc833f73fe7511a2dbff2f0facf2e44dedd41
Vija Wildan Gita Prabawa <vijawildan2@gmail.com>
2024-12-17 17:09:50 +0800

    UI Update

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void PlayButton()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+
+    }
+
+}
